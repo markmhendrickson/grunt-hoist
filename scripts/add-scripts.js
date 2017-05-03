89ec8cf16e24b525ed183b6551b98e7b483ea429
Mark Hendrickson <markmhendrickson@gmail.com>
2017-05-03 16:56:30 +0200

    Move add-scripts to non-Grunt task file

---
+++ scripts/add-scripts.js
@@ -0,0 +1,22 @@
+/**
+ * Add convenience scripts to parent package config file
+ * @module
+ */
+
+'use strict';
+
+var fs = require('fs');
+var Path = require('path');
+
+var path = Path.resolve(__dirname, '../../../package.json'),
+    parentPackage = require(path);
+
+parentPackage.scripts = parentPackage.scripts ? parentPackage.scripts : {};
+
+Object.assign(parentPackage.scripts, {
+  'deploy-all': 'grunt hoist-deploy-all',
+  'deploy-app': 'grunt hoist-deploy-app',
+  'deploy-dependencies': 'grunt hoist-deploy-dependencies'
+});
+
+fs.writeFileSync(path, JSON.stringify(parentPackage, null, 2));
